7fb2962f57568e32957d084f2c752cba83d95f57
Davide Filosa <davideclaypool@gmail.com!>
2022-11-13 11:35:16 +0100

    Fixed bug

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ url = f'https://api.nasa.gov/planetary/apod?api_key={config.api_key}'
 
 request = requests.get(url)
 content = request.json()
+try:
+    title = content['title']
+    image = content['url']
+    description = content['explanation']
+    image_copyright = content['copyright']
+    st.header(title)
+    st.image(image)
+    st.text(f'\u00A9 Copyright: {image_copyright}')
+    st.markdown("***")
+    st.write(description)
+    st.markdown("***")
+except KeyError:
+    title = content['title']
+    image = content['url']
+    description = content['explanation']
+    st.header(title)
+    st.image(image)
+    st.markdown("***")
+    st.write(description)
+    st.markdown("***")
 
-title = content['title']
-image = content['url']
-description = content['explanation']
-image_copyright = content['copyright']
-
-st.header(title)
-st.image(image)
-st.text(f'\u00A9 Copyright: {image_copyright}')
-st.markdown("***")
-st.write(description)
-st.markdown("***")
 
